306964fa3b4ad1f709f025db7264e026fba42f89
Keaton Burleson <keaton@vipindoorstorage.com>
2023-09-21 11:42:16 -0500

    log to stdout

--- util.c
+++ util.c
@@ -221,7 +221,7 @@ void xlog(struct udata *ud, char *fmt, ...)
 	fprintf(fp, "%s %lld pid=%d ", tstamp(now), (long long)now, getpid());
     fprintf( stdout, "%s %lld pid=%d ", tstamp(now), (long long)now, getpid() );
 	va_start(ap, fmt);
-    fprintf( stdout, fmt, ap);
+    vfprintf( stdout, fmt, ap);
 	vfprintf(fp, fmt, ap);
 	fflush(fp);
 	va_end(ap);
