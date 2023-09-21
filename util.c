839ae0b3621d9b8182f423ade4f0d1f2d6edcf86
Keaton Burleson <keaton@vipindoorstorage.com>
2023-09-21 11:43:49 -0500

    revert log to stdout

--- util.c
+++ util.c
@@ -219,9 +219,7 @@ void xlog(struct udata *ud, char *fmt, ...)
 	fp = (ud == NULL) ? stderr : ud->logfp;
 
 	fprintf(fp, "%s %lld pid=%d ", tstamp(now), (long long)now, getpid());
-    fprintf( stdout, "%s %lld pid=%d ", tstamp(now), (long long)now, getpid() );
 	va_start(ap, fmt);
-    vfprintf( stdout, fmt, ap);
 	vfprintf(fp, fmt, ap);
 	fflush(fp);
 	va_end(ap);
